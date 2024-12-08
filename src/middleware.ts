63e9df4052edcb03253fb5bbb1e2e6f585784037
Justin <justin@auroegaming.com>
2024-12-08 14:58:09 +0530

    Fix middleware routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,20 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-
+  const { pathname } = request.nextUrl;
+  if(pathname === '/'){
+    return NextResponse.redirect(new URL('/ar/jo/about', request.url));
+  }
+  if (
+    pathname !== '/' && 
+    !pathname.startsWith('/api') && // API routes
+    !pathname.match(/\/_next/) // Next.js static files (e.g., /_next/static)
+    && !pathname.includes('.js') && !pathname.includes('.ico')
+  ){
+    if(pathname.split('/').length < 4){
+      return NextResponse.redirect(new URL('/ar/jo/'+ pathname.split('/')[pathname.split('/').length - 1], request.url));
+    }
+  }
   // Continue to the requested path if it matches the format
   return NextResponse.next();
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const Home = () => {
+ 
 
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to the homepage
-    router.replace('/ar/jo/about');
-  }, [router]);
-
-  return (
-    <div>
-      <h1>Welcome to the /ar-jo page!</h1>
-      <p>You are currently on: </p>
-    </div>
-  );
-};
-
-export default Home;
+    return (
+      <div>
+        <h1>Welcome to the /ar-jo page!</h1>
+        <p>You are currently on: </p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  

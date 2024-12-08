796c48f188d3dbbb8227ea18b4cb778d0239d558
Justin <justin@auroegaming.com>
2024-12-08 13:38:31 +0530

    Fix dynamic routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home = () => {
- 
 
-    return (
-      <div>
-        <h1>Welcome to the /ar-jo page!</h1>
-        <p>You are currently on: </p>
-      </div>
-    );
-  };
-  
-  export default Home;
-  
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the homepage
+    router.replace('/ar/jo/about');
+  }, [router]);
+
+  return (
+    <div>
+      <h1>Welcome to the /ar-jo page!</h1>
+      <p>You are currently on: </p>
+    </div>
+  );
+};
+
+export default Home;

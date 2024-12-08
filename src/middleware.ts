99c5e19f9c8f94fb4b62701c49b9ff37258ea238
SyedAhmad23 <ahmadshah3236@gmail.com>
2024-12-08 19:51:41 +0500

    fix: fix url issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,67 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  const shouldHandle = pathname.startsWith("/about") || pathname === "/";
-  if (!shouldHandle) {
-    return NextResponse.next();
-  }
+  const defaultLocal = "jo";
+  const defaultLang = "ar";
 
   const segments = pathname.split("/").filter(Boolean);
   const local = segments[0];
   const lang = segments[1];
 
-  if (!local || !lang) {
+  if (pathname === "/") {
     const url = request.nextUrl.clone();
-    url.pathname = `/jo/ar${pathname === "/" ? "" : pathname}`;
+    url.pathname = `/${defaultLocal}/${defaultLang}`;
     return NextResponse.redirect(url);
+  }
+
+  if (segments.length === 0) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/${defaultLocal}/${defaultLang}`;
+    return NextResponse.redirect(url);
+  }
+
+  if (
+    segments.length === 1 &&
+    segments[0] !== defaultLocal &&
+    segments[0] !== defaultLang
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/${defaultLocal}/${defaultLang}/${segments[0]}`;
+    return NextResponse.redirect(url);
+  }
+
+  if (segments.length === 1 && segments[0].length === 2) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/${defaultLocal}/${segments[0]}`;
+    return NextResponse.redirect(url);
+  }
+
+  if (segments.length === 1 && segments[0].length > 2) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/${segments[0]}/${defaultLang}`;
+    return NextResponse.redirect(url);
+  }
+
+  if (segments.length === 2) {
+    if (local === defaultLocal && lang !== defaultLang) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/${defaultLocal}/${defaultLang}`;
+      return NextResponse.redirect(url);
+    } else if (local !== defaultLocal) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/${defaultLocal}/${defaultLang}`;
+      return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
+  }
+
+  if (segments.length > 2) {
+    if (local === lang) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/${defaultLocal}/${defaultLang}`;
+      return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
   }
 
   return NextResponse.next();
